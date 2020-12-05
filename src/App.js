928cf4d0fa3b073b19b38fdc3b2a3e47ccf15101
ralphdanko <danon.rd@gmail.com>
2020-12-05 17:39:47 +0100

    Feat: Add base styles & component project structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Poland from "./svg-map/src/poland-map";
 import RadioSVGMap from './svg-map/src/radio-svg-map';
-import "./svg-map/lib/index.css";
+import Header from './components/header';
+import "./styles/index.scss";
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="t-container">
+        <Header/>
         <RadioSVGMap 
           map={Poland}
         />
---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <div className="t-container">
+          Head
+        </div>
+      </header>
+    );
+  }
+  
+}
+
+export default App;
